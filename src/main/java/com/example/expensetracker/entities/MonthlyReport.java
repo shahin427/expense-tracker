526737c421a4c154d3321c9c1eee1367721638a6
shahin427 <shahinrajaei96@gmail.com>
2025-11-04 12:41:57 +0330

    Monthly report entity created

---
+++ src/main/java/com/example/expensetracker/entities/MonthlyReport.java
@@ -0,0 +1,36 @@
+package com.example.expensetracker.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.YearMonth;
+
+@Entity
+@Table(name = "monthly_reports")
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MonthlyReport {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private YearMonth reportTime;
+
+    @JoinColumn(name = "category_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private CategoryEntity category;
+
+    @Column(nullable = false)
+    private Long totalSpent;
+
+    private Long MonthlyLimit;
+
+    private boolean alertTriggered;
+}
