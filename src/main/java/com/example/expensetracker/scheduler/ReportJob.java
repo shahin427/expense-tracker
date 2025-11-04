27a7b4fb95ba4a8ed85ea6294a639ef54af8555d
shahin427 <shahinrajaei96@gmail.com>
2025-11-04 19:09:27 +0330

    report job scheduler tested

--- src/main/java/com/example/expensetracker/scheduler/ReportJob.java
+++ src/main/java/com/example/expensetracker/scheduler/ReportJob.java
@@ -9,12 +9,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.time.YearMonth;
 import java.util.List;
 
-@Component
+//@Component
+@RestController
 @RequiredArgsConstructor
 @Slf4j
 public class ReportJob {
@@ -22,23 +25,29 @@ public class ReportJob {
     private final ExpenseRepository expenseRepository;
     private final MonthlyReportService monthlyReportService;
 
-    @Scheduled(cron = "0 0 1 1 * ?") // 1st of every month 1 AM
+    //    @Scheduled(cron = "0 0 1 1 * ?") // 1st of every month 1 AM
+    @GetMapping("/run-report")
     public void generateMonthlyReport() {
         YearMonth previousMonth = YearMonth.now().minusMonths(1);
         LocalDateTime startOfTheMonth = previousMonth.atDay(1).atStartOfDay();
         LocalDateTime endOfTheMonth = startOfTheMonth.plusMonths(1).toLocalDate().atStartOfDay();
 
+        log.info("Monthly Report Generated at {} ", LocalDateTime.now());
         List<CategoryEntity> categories = categoryService.getAllCategories();
-
         for (CategoryEntity category : categories) {
-            Long totalSpentAmount = expenseRepository.totalSpentForCategoryBetween(category, startOfTheMonth, endOfTheMonth);
-            MonthlyReport monthlyReport = MonthlyReport.builder()
-                    .reportTime(previousMonth)
-                    .category(category)
-                    .totalSpent(totalSpentAmount)
-                    .monthlyLimit(category.getAlert() != null ? category.getAlert().getMonthlyLimit() : 0)
-                    .build();
-            monthlyReportService.saveMonthlyReport(monthlyReport);
+            try {
+                Long totalSpentAmount = expenseRepository.totalSpentForCategoryBetween(category, startOfTheMonth, endOfTheMonth);
+                MonthlyReport monthlyReport = MonthlyReport.builder()
+                        .reportTime(previousMonth)
+                        .category(category)
+                        .totalSpent(totalSpentAmount)
+                        .monthlyLimit(category.getAlert() != null ? category.getAlert().getMonthlyLimit() : 0)
+                        .build();
+                monthlyReportService.saveMonthlyReport(monthlyReport);
+            } catch (Exception ex) {
+                log.warn("Error handling category {}: {}", category.getName(), ex.getMessage(), ex);                // Do Nothing
+            }
         }
+        log.info("Monthly Report Finished at {}", LocalDateTime.now());
     }
 }
