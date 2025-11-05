076773048e5c4f3cda4a9994095c6ef5361c9988
shahin427 <shahinrajaei96@gmail.com>
2025-11-05 11:45:04 +0330

    token validation is implemented

--- src/main/java/com/example/expensetracker/security/JwtAuthFilter.java
+++ src/main/java/com/example/expensetracker/security/JwtAuthFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -15,6 +16,23 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String path = request.getRequestURI();
+
+        if (path.equals("/user/login")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String auth = request.getHeader("Authorization");
+        String token;
+        String username;
+        if (auth != null && auth.startsWith("Bearer ")) {
+            token = auth.substring(7);
+            username = TokenUtils.tokenValidation(token);
+            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+            }
+        }
 
     }
 }
--- src/main/java/com/example/expensetracker/security/TokenUtils.java
+++ src/main/java/com/example/expensetracker/security/TokenUtils.java
@@ -1,6 +1,9 @@
 package com.example.expensetracker.security;
 
 import com.example.expensetracker.entities.UserEntity;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
@@ -18,12 +21,29 @@ public class TokenUtils {
     public static String generateToken(UserEntity user) {
         return Jwts.builder()
                 .setSubject(user.getUsername())
-                .claim("name",user.getName())
-                .claim("familyName",user.getFamilyName())
+                .claim("name", user.getName())
+                .claim("familyName", user.getFamilyName())
                 .claim("roles", user.getRoles().stream().map(role -> role.getTitle()).toList())
                 .setIssuedAt(new Date())
                 .signWith(key)
                 .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
                 .compact();
+    }
+
+    public static String tokenValidation(String token) {
+
+        try {
+            Claims claims = Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            return claims.getSubject();
+        } catch (ExpiredJwtException e) {
+            throw new RuntimeException("Token is expired");
+        } catch (JwtException e) {
+            throw new RuntimeException("Invalid token");
+        }
     }
 }
